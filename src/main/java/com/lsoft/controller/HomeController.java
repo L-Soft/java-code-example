f2d8c887a1dd76db908417a6cc49087ec547b0e2
lee,taekyang <lty840422@naver.com>
2018-03-09 01:29:45 +0900

    수정

--- src/main/java/com/lsoft/controller/HomeController.java
+++ src/main/java/com/lsoft/controller/HomeController.java
@@ -12,13 +12,13 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Controller
 public class HomeController {
     @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public String index (@RequestParam(value = "name", required = false) String name) {
+    public ModelAndView index (@RequestParam(value = "name", required = false) String name) {
         ModelAndView mv = new ModelAndView("index");
 
         mv.addObject("name", name);
         mv.addObject("message", "Hello world!");
 
-        return "index";
+        return mv;
     }
 
     @RequestMapping(value = "/todoList", method = RequestMethod.GET)
