4a016fc51b8d81a6a05cf38a3fad0073396a239e
lee,taekyang <lty840422@naver.com>
2018-03-09 01:12:55 +0900

    handlebars lib add

--- src/main/java/com/lsoft/controller/HomeController.java
+++ src/main/java/com/lsoft/controller/HomeController.java
@@ -3,16 +3,25 @@ package com.lsoft.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @Controller
 public class HomeController {
-    @RequestMapping("/")
-    public String index (Model model) {
-        model.addAttribute("someAttribute", "someValue");
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index (@RequestParam(value = "name", required = false) String name) {
+        ModelAndView mv = new ModelAndView("index");
+
+        mv.addObject("name", name);
+        mv.addObject("message", "Hello world!");
+
         return "index";
     }
 
-    @RequestMapping("/todoList")
+    @RequestMapping(value = "/todoList", method = RequestMethod.GET)
     public String todoList (Model model) {
         return "todolist";
     }
